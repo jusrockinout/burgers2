1c80eb9e9792c61b258fcbc5c291834cddf08843
jusrockinout <gary.newton@ymail.com>
2017-08-18 23:31:14 -0400

    upload

---
+++ models/burger.js
@@ -0,0 +1,15 @@
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: DataTypes.STRING,
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+      }
+    }
+  });
+  return Burger;
+};
